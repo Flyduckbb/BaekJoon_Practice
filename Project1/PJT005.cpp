50f193e11e43254f1d6019b56d92fed90d6dae10
Flyduck <gogo5208@gmail.com>
2025-03-04 20:36:25 +0900

    미네랄

--- Project1/PJT005.cpp
+++ Project1/PJT005.cpp
@@ -1,5 +1,5 @@
 // 백준 2933번 미네랄
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
@@ -8,161 +8,155 @@
 #include <cstring>
 #include <string>
 #include <cmath>
-
 #define P pair<int, int>
+#define F first
+#define S second
 #define INF 987654321
 
 using namespace std;
-int R, C;
+
+int N, M;
 char map[101][101];
 int check[101][101];
 int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { 1, 0, -1, 0 };
-int Stick_Cnt;
+int K;
 vector<int> Throw;
 vector<P> Move;
 
-// 메모리 초기화 함수
-void reset() {
-	memset(check, 0, sizeof(check));
-	Move.clear();
+void reset_all() {
+    memset(check, 0, sizeof(check));
+    Move.clear();
 }
 
-// 막대기를 던져 미네랄을 파괴하는 함수
-void hit_mineral(int x, int dir) {
-	if (dir % 2 == 0) { // 왼쪽에서 오른쪽
-		for (int y = 1; y <= C; y++) {
-			if (map[R - x + 1][y] == 'x') {
-				map[R - x + 1][y] = '.';
-				break;
-			}
-		}
-	}
-	else { // 오른쪽에서 왼쪽
-		for (int y = C; y >= 1; y--) {
-			if (map[R - x + 1][y] == 'x') {
-				map[R - x + 1][y] = '.';
-				break;
-			}
-		}
-	}
+void break_mineral(int x, int dir) {
+    if (dir % 2 == 0) {
+        int y = 1;
+        while (1) {
+            if (y > M) break;
+            if (map[N - x + 1][y] == 'x') {
+                map[N - x + 1][y] = '.';
+                break;
+            }
+            y++;
+        }
+    }
+    else {
+        int y = M;
+        while (1) {
+            if (y < 1) break;
+            if (map[N - x + 1][y] == 'x') {
+                map[N - x + 1][y] = '.';
+                break;
+            }
+            y--;
+        }
+    }
 }
 
-
-// BFS (너비우선 탐색)
 void bfs(int X, int Y) {
-	queue<P> q;
-	q.push({ X, Y });
-	check[X][Y] = 1;
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int xx = x + dx[i];
-			int yy = y + dy[i];
-			if (xx >= 1 && xx <= R && yy >= 1 && yy <= C) {
-				if (check[xx][yy] == 0 && map[xx][yy] == 'x') {
-					check[xx][yy] = 1;
-					q.push({ xx, yy });
-				}
-			}
-		}
-	}
+    queue<P> q;
+    q.push({ X, Y });
+    check[X][Y] = 1;
+    while (!q.empty()) {
+        int x = q.front().F;
+        int y = q.front().S;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if (xx >= 1 && xx <= N && yy >= 1 && yy <= M) {
+                if (check[xx][yy] == 0 && map[xx][yy] == 'x') {
+                    check[xx][yy] = 1;
+                    q.push({ xx, yy });
+                }
+            }
+        }
+    }
 }
 
-// 클러스터를 바닥으로 떨어뜨리는 함수
-int drop_Cluster(int x, int y) {
-	int cnt = 0;
-	for (int i = x + 1; i <= R; i++) {
-		if (map[i][y] == 'x') {
-			if (check[i][y] == 0)
-				return INF;
-			else
-				return cnt + i;
-		}
-		return cnt;
-	}
+int move_cluster(int x, int y) {
+    int cnt = 0;
+    for (int i = x + 1; i <= N; i++) {
+        if (map[i][y] == 'x') {
+            if (check[i][y] == 0) return INF; //이미 아래에 움직일 미네랄이 있음으로 이번은 X
+            else return cnt;
+        }
+        else cnt++;
+    }
+    return cnt;
 }
 
-//지도 구현하는 함수
 void remake_map() {
-	int down = INF;
-	for (int i = 0; i < Move.size(); i++) {
-		int cnt = drop_Cluster(Move[i].first, Move[i].second);
-		if (cnt == INF)
-			continue;
-		else
-			down = min(cnt, down);
-	}
-	if (down == INF)
-		return;
-	for (int i = 0; i < Move.size(); i++) {
-		int x = Move[i].first, y = Move[i].second;
-		map[x][y] = '.';
-	}
-	for (int i = 0; i < Move.size(); i++) {
-		int x = Move[i].first, y = Move[i].second;
-		map[x + down][y] = 'x';
-	}
+    int down = INF;
+    for (int i = 0; i < Move.size(); i++) {
+        int cnt = move_cluster(Move[i].F, Move[i].S);
+        if (cnt == INF) continue;
+        else down = min(cnt, down);
+    }
+    if (down == INF) return;
+    for (int i = 0; i < Move.size(); i++) {
+        int x = Move[i].F, y = Move[i].S;
+        map[x][y] = '.';
+    }
+    for (int i = 0; i < Move.size(); i++) {
+        int x = Move[i].F, y = Move[i].S;
+        map[x + down][y] = 'x';
+    }
 }
 
-// 공중에 떠 있는 클러스터 찾는 함수
-bool find_cluster() {
-	bool can = false;
-	for (int i = 1; i <= C; i++) {
-		if (check[R][1] == 0 && map[R][i] == 'x') bfs(R, i);
-	}
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			if (map[i][j] == 'x' && check[i][j] == 0) {
-				Move.push_back({ i,j });
-				can = true;
-			}
-		}
-	}
-	return can;
+bool find_float_cluster() {
+    bool can = false; // 공중에 떠있는 미네랄이 없는 경우, false를 return 해준다.
+    for (int i = 1; i <= M; i++) {
+        if (check[N][i] == 0 && map[N][i] == 'x') bfs(N, i);
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            if (map[i][j] == 'x' && check[i][j] == 0) { // 공중에 떠있는 미네랄
+                Move.push_back({ i,j }); // 움직이기 위해 좌표를 저장한다.
+                can = true;
+            }
+        }
+    }
+    return can;
 }
 
-// 지도 출력하는 함수
 void show_map() {
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			cout << map[i][j];
-		}
-		cout << "\n";
-	}
-	cout << "\n";
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            cout << map[i][j];
+        }
+        cout << "\n";
+    }
+    cout << "\n";
 }
 
 void solve() {
-	for (int i = 0; i < Throw.size(); i++) {
-		reset();
-		hit_mineral(Throw[i], i);
-		if (!find_cluster())
-			continue;
-		remake_map();
-	}
-	show_map();
+    for (int i = 0; i < Throw.size(); i++) {
+        reset_all();
+        break_mineral(Throw[i], i);
+        if (!find_float_cluster()) continue;
+        remake_map();
+    }
+    show_map();
 }
 
+
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	
-	cin >> R >> C;
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			cin >> map[i][j];
-		}
-	}
-	
-	cin >> Stick_Cnt;
-	for (int i = 1; i <= Stick_Cnt; i++) {
-		int x;
-		cin >> x;
-		Throw.push_back(x);
-	}
-	solve();
-	return 0;
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            cin >> map[i][j];
+        }
+    }
+    cin >> K;
+    for (int i = 1; i <= K; i++) {
+        int x;
+        cin >> x;
+        Throw.push_back(x);
+    }
+    solve();
+    return 0;
 }
